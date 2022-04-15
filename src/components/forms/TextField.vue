09350c42a963b2b625aaa28b2e7f009b73b3ecc2
Warsono <warsono16694@gmail.com>
2022-04-15 21:32:47 +0700

    feat: add form example

---
+++ src/components/forms/TextField.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useField } from "vee-validate";
+
+const props = defineProps<{
+  name: string;
+  label?: string;
+  rules?: string
+}>()
+
+const { value, errorMessage } = useField(props.name, props.rules);
+</script>
+
+<template>
+  <div>
+    <label :for="name">{{ label }}</label>
+    <input :id="name" type="text" v-model="value" v-bind="$attrs" />
+    <div>{{ errorMessage }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import { Form } from 'vee-validate';
+import TextField from '@/components/forms/TextField.vue';
+import { object, string } from 'yup';
+
+const schema = object({
+  name: string().required(),
+});
+
+const onSubmit = (values: Record<string, any>) => {
+  console.log(values);
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Form @submit="onSubmit" :validation-schema="schema">
+      <TextField label="Name" name="name" placeholder="Name" />
+      <TextField label="Email" name="email" placeholder="Email" />
+      <TextField label="Password" name="password" type="password" />
+
+      <button>Submit</button>
+    </Form>
   </main>
 </template>
